aba2c31cd7ac4f8b4d6c71686c8c384cd9b3b7fe
Karla Gardiner <karla.gardiner@gmail.com>
2020-07-07 10:40:52 +0100

    Added my profile to main page

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-// import classes from './App.module.css';
+import classes from './App.module.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Karla Gardiner</h1>
-      <p>Econometrician turned Computer Scientist</p>
-      <p>See my <a href='https://github.com/KG700/CV'>CV</a> to find out more about me</p>
+    <div className={classes.App}>
+      <div className={classes.Container}>
+        <h1>Karla Gardiner</h1>
+        <h3>Econometrician turned Computer Scientist</h3>
+        <p>After 8 years as an Econometrician I'm taking the leap into Software Development.
+          I'm excited about turning my hobby into my career, and the endless potential to learn in this fast-changing industry.</p>
+          <p>I am an enthusiastic, positive and self-motivated person, who enjoys taking on new challenges. Check out out my <a href='https://github.com/KG700/CV'>CV</a> to find out more</p>
+      </div>
     </div>
   );
 }
