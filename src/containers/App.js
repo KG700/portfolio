f61c2fe9a0ac0508658c59aa3dd6ae865ec41fd6
Karla Gardiner <karla.gardiner@gmail.com>
2020-07-07 09:39:47 +0100

    Set up mvp including my name, and a link to my github CV

---
+++ src/containers/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import classes from './App.module.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Karla Gardiner</h1>
+      <p>Econometrician turned Computer Scientist</p>
+      <p>See my <a href='https://github.com/KG700/CV'>CV</a> to find out more about me</p>
+    </div>
+  );
+}
+
+export default App;
