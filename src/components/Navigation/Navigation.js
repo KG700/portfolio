427e4239d387312b5a1e7a93b62e47bf1723f9b9
Karla Gardiner <karla.gardiner@gmail.com>
2020-07-08 20:52:20 +0100

    Added Card and Navigation components

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import classes from './Navigation.module.css';
+
+const Navigation = () => {
+  return (
+    <nav className={classes.Navigation}>
+      <ul>
+        <li>EXPERIENCE</li>
+        <li>EDUCATION</li>
+        <li>PROJECTS</li>
+        <li>LEARNING</li>
+      </ul>
+    </nav>
+  )
+};
+
+export default Navigation;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,17 +1,26 @@
 import React from 'react';
+
 import classes from './App.module.css';
+import Card from '../components/Card/Card';
+import Navigation from '../components/Navigation/Navigation';
+
 
 function App() {
   return (
+    <>
+    <Navigation />
     <div className={classes.App}>
       <div className={classes.Container}>
         <h1>Karla Gardiner</h1>
         <h3>Econometrician turned Computer Scientist</h3>
-        <p>After 8 years as an Econometrician I'm taking the leap into Software Development.
-          I'm excited about turning my hobby into my career, and the endless potential to learn in this fast-changing industry.</p>
+        <Card>
+          <p>After 8 years as an Econometrician I'm taking the leap into Software Development.
+            I'm excited about turning my hobby into my career, and the endless potential to learn in this fast-changing industry.</p>
           <p>I am an enthusiastic, positive and self-motivated person, who enjoys taking on new challenges. Check out out my <a href='https://github.com/KG700/CV'>CV</a> to find out more</p>
+        </Card>
       </div>
     </div>
+    </>
   );
 }
 
