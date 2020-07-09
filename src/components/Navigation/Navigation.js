7d1ef80b6ccd6da4cedb3417885a471863283404
Karla Gardiner <karla.gardiner@gmail.com>
2020-07-09 12:29:51 +0100

    Commented out Education, Project and Learning links. Will be added in when I add content to these pages

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -16,9 +16,11 @@ const Navigation = () => {
       <ul>
         <li><NavLink to='/' exact activeClassName={classes.active}>HOME</NavLink></li>
         <li><NavLink to='/work-experience' activeClassName={classes.active}>EXPERIENCE</NavLink></li>
+        {/*}
         <li><NavLink to='/education' activeClassName={classes.active}>EDUCATION</NavLink></li>
         <li><NavLink to='/projects' activeClassName={classes.active}>PROJECTS</NavLink></li>
         <li><NavLink to='/learning' activeClassName={classes.active}>LEARNING</NavLink></li>
+        */}
       </ul>
     </nav>
   )
