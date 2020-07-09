999d435734b157bf2d3ec67e12fb5e871a84c36d
Karla Gardiner <karla.gardiner@gmail.com>
2020-07-09 11:51:06 +0100

    Added styling for navigation links onhover

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import classes from './Navigation.module.css';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 import Home from '../../containers/pages/Home/Home';
 import WorkExperience from '../../containers/pages/WorkExperience/WorkExperience';
@@ -10,8 +10,8 @@ const Navigation = () => {
   return (
     <nav className={classes.Navigation}>
       <ul>
-        <li><Link to='/'>HOME</Link></li>
-        <li><Link to='/work-experience'>EXPERIENCE</Link></li>
+        <li><NavLink to='/'>HOME</NavLink></li>
+        <li><NavLink to='/work-experience'>EXPERIENCE</NavLink></li>
         <li>EDUCATION</li>
         <li>PROJECTS</li>
         <li>LEARNING</li>
